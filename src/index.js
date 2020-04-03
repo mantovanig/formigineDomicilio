7ae29310ee829fb312d5fb455c9893e6a2c7df85
mantovanig <gabriele.mantovani92@gmail.com>
2020-04-03 22:22:15 +0200

    remove debug tooltip

--- src/index.js
+++ src/index.js
@@ -146,7 +146,7 @@ export default class App extends Component {
             />
             <PWAPrompt
                timesToShow={3}
-               debug={true}
+               // debug={true}
                copyTitle="Sono un'app!"
                copyBody="Aggiungimi alla home per utilizzarmi in fullscreen e offline. Così appena vorrai ordinare mi avrai a portata!"
                copyShareButtonLabel="Fai tap sul bottone condividi"
