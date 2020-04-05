0c1c3647e295dfd328e558f080b7c3e6a828ded9
mantovanig <gabriele.mantovani92@gmail.com>
2020-04-05 22:57:23 +0200

    remove google link

--- src/components/lisItem.js
+++ src/components/lisItem.js
@@ -5,6 +5,7 @@ import {
    Mail as IconMail,
    Globe as IconGlobe,
 } from "preact-feather";
+import _isEmpty from "lodash.isempty";
 
 // Actions
 import { Action } from "../index";
@@ -12,9 +13,6 @@ import { Action } from "../index";
 export const ListItem = ({ name, tel, site, mail, note }) => {
    const [infoVisible, setInfoVisible] = useState(false);
    const action = useContext(Action);
-   const encodedName = encodeURIComponent(name);
-   const encodedCity = encodeURIComponent(process.env.PREACT_APP_CITY);
-   const searchUrl = `https://www.google.com/search?q=${encodedName}%20${encodedCity}`;
 
    function handleClick() {
       setInfoVisible(!infoVisible);
@@ -23,16 +21,9 @@ export const ListItem = ({ name, tel, site, mail, note }) => {
    return (
       <div class="rounded-lg border bg-gray-200 p-4 md:p-5 my-5 text-md lg:text-xl font-semibold text-gray-700">
          <div class="flex justify-between items-center">
-            <span>
-               <a
-                  class="hover:underline"
-                  href={searchUrl}
-                  target="_blank"
-                  rel="noopener noreferrer"
-               >
-                  {name}
-               </a>
-            </span>
+            <a href="#">
+               <span onClick={handleClick}>{name}</span>
+            </a>
             <div class="flex">
                {note && (
                   <span
@@ -86,7 +77,7 @@ export const ListItem = ({ name, tel, site, mail, note }) => {
                )}
             </div>
          </div>
-         {infoVisible && (
+         {infoVisible && !_isEmpty(note) && (
             <div class="block mt-10">
                <p class="text-yellow-700 text-sm md:text-md lg:text-lg">
                   {note}
